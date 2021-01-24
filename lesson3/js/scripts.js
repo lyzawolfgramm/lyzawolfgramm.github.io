8dd288ce2e36e8c4e5e1f9ac82e904e3c7ebd0e6
lyzawolfgramm <lyzawolfgramm@gmail.com>
2021-01-23 17:32:26 -0700

    commit

---
+++ lesson3/js/scripts.js
@@ -0,0 +1,35 @@
+
+//nav
+function myFunction() {
+    var x = document.getElementById("centered_nav");
+    if (x.className === "rc_nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "rc_nav";
+    }
+}
+
+
+
+//updated
+let LastModified = new Date();  
+document.getElementById("currentDate").innerHTML = LastModified;
+
+
+//table
+var headertext = [],
+headers = document.querySelectorAll("#miyazaki th"),
+tablerows = document.querySelectorAll("#miyazaki th"),
+tablebody = document.querySelector("#miyazaki tbody");
+
+for(var i = 0; i < headers.length; i++) {
+  var current = headers[i];
+  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+} 
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute("data-th", headertext[j]);
+  } 
+}
+
+
