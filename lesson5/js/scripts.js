caaacffc1bbfc8c8f59ed08c8fbe0d0152b3b7c9
lyzawolfgramm <lyzawolfgramm@gmail.com>
2021-02-10 19:30:54 -0700

    commit

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -6,3 +6,11 @@ function toggleMenu() {
 //updated Date
 let LastModified = new Date();  
 document.getElementById("currentDate").innerHTML = LastModified;
+
+
+const completeDate = new Date();
+let today = completeDate.getDay();
+let element = document.getElementById('pancakeBanner');
+if (today == 6) {
+    element.style.display = "block";
+}
