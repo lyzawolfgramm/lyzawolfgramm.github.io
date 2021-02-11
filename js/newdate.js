7cc23964b1746be599061180c5c2161abac73f9c
lyzawolfgramm <lyzawolfgramm@gmail.com>
2021-02-11 15:09:42 -0700

    commit

--- js/newdate.js
+++ js/newdate.js
@@ -1,2 +1,13 @@
 let LastModified = new Date();  
 document.getElementById("currentDate").innerHTML = LastModified;
+
+
+
+//Webloader 
+WebFont.load({
+  google: {
+    families: [
+       'Meddon'
+    ]
+  }
+});
