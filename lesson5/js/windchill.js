8ea2a5a0e6e02640d8cef59e7d9c418e555e7cd9
lyzawolfgramm <lyzawolfgramm@gmail.com>
2021-02-13 22:51:47 -0700

    committ

--- lesson5/js/windchill.js
+++ lesson5/js/windchill.js
@@ -2,20 +2,16 @@ let t = parseFloat(document.getElementById('temp').inner.HTML);
 let w = parseFloat(document.getElementById('wind').inner.HTML);
 let a = calc(t,w);
 
-fuction calc(a,b) {
-	let result = 35.74 + (0.6215 * a) 
-	- (35.75 * Math.pow(b, 0.16)) 
-	+ (0.4275 * a * Math.pow(b, 0.16));
+fuction calc(x,y) {
+	let result = 35.74 + (0.6215 * x) 
+	- (35.75 * Math.pow(y, 0.16)) 
+	+ (0.4275 * x * Math.pow(y, 0.16));
 	return result.toFixed(1);
-
 }
 
-if (t <= 50 && w > 3) {
+if(t <= 50 && w > 3) {
 	let finalResult = `${a}°F`
 }
 else {
 	let finalResult = 'N/A'
 }
-
-document.getElementById('windchill').innerHTML = finalResult;
-
