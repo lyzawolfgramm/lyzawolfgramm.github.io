63183448c369dd4092c0bdb000ca7d83e3d961a4
lyzawolfgramm <lyzawolfgramm@gmail.com>
2021-02-20 12:27:40 -0700

    commit

---
+++ lesson7/js/scripts.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+}
+
+
+//updated Date
+let LastModified = new Date();  
+document.getElementById("currentDate").innerHTML = LastModified;
+
+//banner
+const completeDate = new Date();
+let today = completeDate.getDay();
+let element = document.getElementById('pancakeBanner');
+if (today == 6) {
+    element.style.display = "block";
+}
+
+// Fonts 
+WebFont.load({
+    google: {
+      families: ['Cedarville Cursive', 'Cabin']
+    }
+  });
+
+
+
