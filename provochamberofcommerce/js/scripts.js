eb33fb64fb6e82796ad53c0d3e33bc995ef044f3
lyzawolfgramm <77038780+lyzawolfgramm@users.noreply.github.com>
2021-04-07 23:38:32 -0600

    Add files via upload

---
+++ provochamberofcommerce/js/scripts.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+}
+
+
+// Fonts 
+WebFont.load({
+    google: {
+      families: ['Montserrat', 'Inter', 'Open Sans', 'Chivo']
+    }
+  });
